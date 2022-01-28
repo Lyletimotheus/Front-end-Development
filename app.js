858a1cf9ffc8cf7dd29f78b604d3ad1ad9091c32
Lyle Timotheus <lyle6494@gmail.com>
2022-01-28 16:03:22 +0200

    Event modifiers

---
+++ app.js
@@ -0,0 +1,32 @@
+// Vue Instance to control the entire application
+new Vue({
+    el: "#vue-app", // Referencing the id in HTML
+    data: {
+        name: "Lyle", // Passing in data we can access from HTML 
+        job: "Web Developer",
+        website: "https://www.ciphercode.co.za",
+        websiteTag:"<a href='https://www.google.co.za'>Google SA</a>",
+        age: 32,
+        x:0,
+        y:0
+    },
+    methods: {
+        greet: function(time) {
+            return "Good " + time + ", " + this.name;
+        },
+        add:function(inc) {
+            this.age += inc;
+        },
+        subtract:function(dec) {
+            this.age -= dec;
+        },
+        updateXY:function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function() {
+            alert("You've clicked me!");
+        }
+
+    },
+});
