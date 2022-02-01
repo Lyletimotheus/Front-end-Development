9c1a0cd53556a7268a76c6a6d28c0a175c38777b
Lyle Timotheus <lyle6494@gmail.com>
2022-02-01 14:09:30 +0200

    Using the Vue CLI to setup

---
+++ Vue 2 JS/vue-js-playlist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+// Nesting a variable locally
+import Ninjas from './Ninjas.vue'
+
+export default {
+  components: { // Referencing the component locally 
+    'ninjas': Ninjas
+  },
+  data() {
+    return {
+      title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+<style>
+  h1{
+    color: purple;
+  }
+</style>
